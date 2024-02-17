6d71119fd3e7e99830ac2a1f719efd1e27100812
qubifer <148722588+qubifer@users.noreply.github.com>
2024-02-16 22:33:21 -0500

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,33 +1,32 @@
 const { Client, Intents } = require('discord.js');
-const sqlite = require('sqlite');
+const mysql = require('mysql');
+const databaseConfig = require("../config");
 
-
-const databaseFilename = '../dautoban.sqlite';
 module.exports = {
     name: 'guildMemberAdd',
     async execute(member) {
         try {
- 
-            const db = await sqlite.open({
-                filename: databaseFilename,
-                driver: require('sqlite3').Database
+            const connection = mysql.createConnection(databaseConfig);
+            connection.connect();
+
+            const query = 'SELECT * FROM autoban WHERE user_id = ?';
+            const [bannedUser] = await new Promise((resolve, reject) => {
+                connection.query(query, [member.id], (error, results) => {
+                    if (error) reject(error);
+                    else resolve(results);
+                });
             });
 
-            const bannedUser = await db.get('SELECT * FROM autoban WHERE user_id = ?', [member.id]);
-    
             if (bannedUser) {
-       
-                await member.ban({ reason: `Auto-banned: ${bannedUser.reason}` });
-    
-                console.log(`User ${member.user.tag} (${member.id}) was auto-banned from guild ${member.guild.name}.`);
-
-                await db.close();
+                await member.ban({ reason: `Auto-Blacklisted: ${bannedUser.reason}` });
+                console.log(`User ${member.user.tag} (${member.id}) was banned (Auto Blacklisted) from guild ${member.guild.name}.`);
+                connection.end();
                 return;
             }
-    
-      
-            await db.close();
+
+            connection.end();
         } catch (error) {
             console.error('Error banning user:', error);
         }
-    }}    
+    },
+};
